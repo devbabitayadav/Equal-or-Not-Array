d8fab675b47c72b3fd4339c772ffbd7611c0526b
“devbabitayadav” <babitayadav9584@gmail.com>
2023-07-01 14:01:16 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const array1 = [1, 2, 3];
+const array2 = [1, 2, 3];
+
+const equalValues = (array1. length === array2. length); 
+if (equalValues) {
+document.write("<h2>The arrays have equal values.");
+} else {
+document.write("<h2>The arrays do not have equal values.");
+}
+
+
